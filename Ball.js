5896190aa2f172b08f6696cc21b19ea441d7a711
somedude234 <82318113+somedude234@users.noreply.github.com>
2021-06-26 18:40:30 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,25 @@
+class Ball{
+    constructor(x, y, width, height) {
+        var options = {
+            'frictionAir':0.005,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        //this.image = loadImage("sprites/base.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+      //  imageMode(CENTER);
+       // image(this.image, 0, 0, this.width, this.height);
+        ellipseMode(CENTER);
+        ellipse(0,0,this.width,this.height);
+
+        pop();
+      }
+}
